521ef91f40ba5b27d848dd736515a8ac40832321
cattys <cattys0916@gmail.com>
2020-11-09 03:18:19 +0900

    JS: Projects filtering

--- main.js
+++ main.js
@@ -49,13 +49,91 @@ homeContactBtn.addEventListener('click', ()=>{
 
 
 // Make home slowly fade to transparent as the window scroll down
-const home =document.querySelector('.home__container');
+const home = document.querySelector('.home__container');
 const homeHeight =home.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
     home.style.opacity=(1- window.scrollY/homeHeight)
 });
 
+// 내가 만든거
+// const arrowBtn = document.querySelector('.arrow-up');
+// window.addEventListener('scroll', ()=>{
+//     if(window.scrollY > homeHeight){
+//         arrowBtn.style.display = 'flex'
+//     } else {
+//         arrowBtn.style.display = 'none'
+//     }
+// })
+
+// Show "arrow up" button when scrolling down
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', ()=>{
+    if(  window.scrollY> homeHeight/2){
+        arrowUp.classList.add('visible');
+    } else {
+        arrowUp.classList.remove('visible');
+    }
+})
+
+/* Handle click on "arrow up" button */
+arrowUp.addEventListener('click', ()=>{
+   scrollIntoView('#home');
+})
+
+// Projects
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+
+workBtnContainer.addEventListener('click', (e)=>{
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    // console.log(filter);
+    if(filter==null){
+        return;
+    }
+
+    projectContainer.classList.add('anime-out');
+
+    // 같은거 1
+
+    setTimeout(() => {
+        projects.forEach( (project)=>{
+            // console.log(project);
+            console.log(project.dataset.type);
+            if(filter ==='*' || filter === project.dataset.type){
+                project.classList.remove('invisible');
+            } else {
+                project.classList.add('invisible');
+            }
+        })
+        projectContainer.classList.remove('anime-out');
+    }, 300);
+
+    
+    // 같은거 2
+    for(let project of projects){
+        // console.log(project);
+    }
+    // 같은거 3
+    let project;
+    for(let i=0; i<projects.length; i++){
+        project =projects[i];
+        // console.log(project);
+    }
+})
+
+
+
 function scrollIntoView(selector){
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior: "smooth"});
 }
+
+// var dbd = document.querySelectorAll('.category__btn');
+// for(var i in dbd){
+//     console.log(dbd[i])
+//     var b = dbd[i];
+//     b.addEventListener('click', ()=>{
+//         console.log(b.dataset.filter);
+//     })
+// }
